23e57b0375fc8bf29081e544ff72a56c88e7584f
Alex Prevot <alex1.prevot@epitech.eu>
2022-11-10 19:14:51 +0100

    aa

--- front/src/pages/home.tsx
+++ front/src/pages/home.tsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, Center } from "@chakra-ui/react"
+import { Identity } from "@semaphore-protocol/identity"
 
-const Home = (): JSX.Element => {
+const Home = () => {
+    // const { trapdoor, nullifier, commitment } = new Identity()
+    const [test, setTest] = useState("");
+    const onClick = () => {
+        console.log("okk")
+        setTest("yo")
+        
+        // const identity = new Identity()
+        // console.log(identity.toString())
+        console.log(test)
+        // const identity2 = new Identity(identity.toString())
+        // console.log(identity2)
+    };
+
     return(
         <>
+            <Center>
+                <Button onClick={onClick}>Test</Button>
+            </Center>
         </>
-    )
-}
+    );
+};
 
 export default Home;
